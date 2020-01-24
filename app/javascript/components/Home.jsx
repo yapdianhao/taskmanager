d1a083cb8a8e97995afb883ad6be0c1cffd07b64
yapdianhao1011 <yapdhao@gmail.com>
2020-01-25 05:04:15 +0800

    fixed Home frontend

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../assets/stylesheets/homepage.scss";
 
 export default () => (
-  <div className=
-  "vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+  //<div className=
+  //"vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
   <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">To-dos</h1>
-        <p className="lead">
-          A list of to-do tasks
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/tasks"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          View To-do Tasks
+    <div className="container secondary-color">
+      <h1 className="display-4">To-dos</h1>
+      <p className="lead">Your task manager to address your procrastination</p>
+      <hr className="my-4" />
+      <Link to="/tasks" className="btn btn-outline-primary waves-effect">
+        View To-do Tasks
+      </Link>
+      <hr className="my-4" />
+      <div>
+        <Link to="/Search" className="btn btn-outline-danger">
+          Search bar
         </Link>
-        <div>
-            <Link to="/Search">
-                Search here
-            </Link>
-        </div>
+      </div>
+      <hr className="my-4" />
+      <div>
+        <Link to="/Task" className="btn btn-outline-warning">
+          Add Task
+        </Link>
       </div>
     </div>
   </div>
+  //</div>
 );
