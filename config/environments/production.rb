23a155865048928c8f3fde9111448fe2140e751a
yapdianhao1011 <yapdhao@gmail.com>
2020-02-06 23:54:26 +0800

    added lines for css in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  
+  config.serve_static_assets = true
+
+  config.assets.compile = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -27,7 +31,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
