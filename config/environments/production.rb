678df1f5f7e06dc083d8a4e5b7100d905acd765f
yapdianhao1011 <yapdhao@gmail.com>
2020-02-07 01:15:05 +0800

    changed config.assets.compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   #config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.serve_static_assets = true
   config.assets.digest = true
 
