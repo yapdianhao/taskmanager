a987b00beba1d18b940ee4b57add6fb985f88c57
yapdianhao1011 <yapdhao@gmail.com>
2020-02-07 16:41:27 +0800

    modified bootstrap import

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <App />,
-    document.body.appendChild(document.createElement("div"))
-  );
+  render(<App />, document.body.appendChild(document.createElement("div")));
 });
