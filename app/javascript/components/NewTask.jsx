7fd9ebdf7b91255ab83dc76f2b49a3c3ccad475a
yapdianhao1011 <yapdhao@gmail.com>
2020-02-09 21:42:31 +0800

    fixed create task background

--- app/javascript/components/NewTask.jsx
+++ app/javascript/components/NewTask.jsx
@@ -100,7 +100,7 @@ class NewTask extends React.Component {
    */
   render() {
     return (
-      <div className="background full">
+      <div className="background">
         <div>
           <Link
             to="/"
